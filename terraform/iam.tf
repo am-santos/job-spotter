28d5d5d7059ab82c650f085924cb95920880758a
am-santos <59169294+am-santos@users.noreply.github.com>
2026-01-22 23:53:38 +0000

    (terraform.iam) added missing permissions

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -17,3 +17,17 @@ resource "google_project_iam_member" "cloudsql_client" {
   role    = "roles/cloudsql.client"
   member  = "serviceAccount:${google_service_account.gsa.email}"
 }
+
+# Cloud Run Developer role (required to deploy Cloud Run services)
+resource "google_project_iam_member" "run_developer" {
+  project = var.project_id
+  role    = "roles/run.developer"
+  member  = "serviceAccount:${google_service_account.gsa.email}"
+}
+
+# Service Account User role (required to deploy as the service account)
+resource "google_project_iam_member" "sa_user" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.gsa.email}"
+}
