4e89ffa085941fa1829ce458f6ea4ac229a5200c
am-santos <59169294+am-santos@users.noreply.github.com>
2026-01-24 00:26:37 +0000

    (deploy) remove dedicated worker service deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -e
 
-if [ "$SERVICE_ROLE" = "worker" ]; then
-    echo "Running as Worker (Celery)..."
-    echo "Celery is not configured yet. Skipping command: 'exec celery -A config worker --loglevel=info'"
-    #exec celery -A config worker --loglevel=info
-else
-    # Run migrations
-    echo "Applying database migrations..."
-    python manage.py migrate
+# Run migrations
+echo "Applying database migrations..."
+python manage.py migrate
 
-    # Execute the command passed to the docker container
-    echo "Executing command: $@"
-    exec "$@"
-fi
+# Execute the command passed to the docker container
+echo "Executing command: $@"
+exec "$@"
